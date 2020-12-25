0a0ddcab060ac644cce0a1fa984482f5a88646d7
Piyush <piyush@example.com>
2020-12-26 00:19:07 +0530

    another commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <Footer/>
+      <Footer />
 
     </div>
   );
