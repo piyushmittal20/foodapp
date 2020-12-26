5d808a12bd42fccea0cbf8cb8cce69055fec68c6
Piyush <piyush@example.com>
2020-12-26 11:11:52 +0530

    search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-
 import Header from './components/Header/Header';
+import Searchbar from './components/Search/SearchBar';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App" >
       <Header />
+      <Searchbar />
       <Footer />
 
     </div>
---
+++ frontend/src/components/Search/SearchBar.js
@@ -0,0 +1,27 @@
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return (
+        <>
+            <div className='search_bar'>
+                <Paper component="form" className='textField' elevation={3}>
+                    <InputBase
+                        className='input'
+                        placeholder="Search Your favorite Restaurant..."
+                        inputProps={{ 'aria-label': 'search your favorite restaurant' }}
+                        onC
+                    />
+                    <IconButton type="submit" className='iconButton' aria-label="search">
+                        <SearchIcon className='search-icon' />
+                    </IconButton>
+                </Paper>
+            </div>
+        </>
+    )
+}
+
+export default SearchBar
