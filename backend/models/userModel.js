1e89876b602c64fac2d1e7a28a20dd494327c680
Piyush <piyush@example.com>
2020-12-27 14:12:36 +0530

    model complete

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,23 +14,21 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    
-    address:deliveryInfo,
-
-account: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
-cart: {
-    items: [
-    {
-    _id: false,
-    itemId: {
-        type: Schema.Types.ObjectId,
-        ref: "Item",
-        required: true,
+    address: deliveryInfo,
+    account: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+    cart: {
+        items: [
+            {
+                _id: false,
+                itemId: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Item",
+                    required: true,
+                },
+                quantity: { type: Number, required: true },
+            },
+        ],
     },
-    quantity: { type: Number, required: true },
-    },
-],
-},
 },
     {
         timestamps: true
